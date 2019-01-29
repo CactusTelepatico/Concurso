97cfd615775e52898bee89b25610ecb6d1af2982
Alejandro <asenorsnz@gmail.com>
2019-01-29 10:54:54 +0100

    Ejercicio DNI apañado

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,23 +1,42 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-	public static void main(String[] args) {
-		String DNI;
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		DNI = sc.nextLine();
-		// Pasar la letra de char a string
-		char letra = DNI.charAt(8);
-		String numeros = DNI.substring(0, DNI.length() - 1);
-		int numeros1 = Integer.parseInt(numeros);
-		int resto = numeros1 % 23;
-		String mapa = "TRWAGMYFPDXBNJZSQVHLCKE";
-		char posicion = mapa.charAt(resto);
-		if (posicion == letra) {
-			System.out.println("DNI valido");
-		} else {
-			System.out.println("DNI no valido");
+		Scanner scv = new Scanner(System.in);
+		String DNI;
+		int valor;
+		valor = scv.nextInt();
+		for (int i = 0; i < valor; i++) {
+			DNI = sc.nextLine();
+			// Pasar la letra de char a string
+			char letra = 0;
+			String numeros = DNI.substring(0, DNI.length() - 1);
+			int numeros1 = 0;
+			try {
+				numeros1 = Integer.parseInt(numeros);
+			} catch (NumberFormatException excepcion) {
+				System.out.println("DNI muy largo");
+			}
+			try {
+				letra = DNI.charAt(8);
+			} catch (IndexOutOfBoundsException excepcion2) {
+				System.out.println("DNI muy corto");
+			}
+			int resto = numeros1 % 23;
+			String mapa = "TRWAGMYFPDXBNJZSQVHLCKE";
+			char posicion = mapa.charAt(resto);
+			if (posicion == letra) {
+				System.out.println("DNI valido");
+			} else {
+				System.out.println("DNI no valido");
+			}
 		}
-		sc.close();
+
 	}
 }
