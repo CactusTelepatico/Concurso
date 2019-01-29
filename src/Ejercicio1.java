9b885b241a082d7f9b1a8d79ba7d54627791348b
Alfredo Manjon <manjon.canela@gmail.com>
2019-01-29 09:50:43 +0100

    Añadidio ejercicio 1 beta

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,4 +1,23 @@
 
-public class Ejercicio1 {
+import java.util.Scanner;
 
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		String DNI;
+		Scanner sc = new Scanner(System.in);
+		DNI = sc.nextLine();
+		// Pasar la letra de char a string
+		char letra = DNI.charAt(8);
+		String numeros = DNI.substring(0, DNI.length() - 1);
+		int numeros1 = Integer.parseInt(numeros);
+		int resto = numeros1 % 23;
+		String mapa = "TRWAGMYFPDXBNJZSQVHLCKE";
+		char posicion = mapa.charAt(resto);
+		if (posicion == letra) {
+			System.out.println("DNI valido");
+		} else {
+			System.out.println("DNI no valido");
+		}
+		sc.close();
+	}
 }
